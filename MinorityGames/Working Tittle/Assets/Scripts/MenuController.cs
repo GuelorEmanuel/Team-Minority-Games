f72571bdb4d039cadb76d861f67a9465e5962aef
Marcio <marciozkim@gmail.com>
2015-11-22 19:45:02 -0500

    Scene transitions

---
+++ MinorityGames/Working Tittle/Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public void LoadScene(string sceneName){
+		if (sceneName.Equals ("Exit")) {
+			Debug.Log ("Quit Called");
+			Application.Quit ();
+		} else {
+			Debug.Log ("Load : " + sceneName);
+			Application.LoadLevel (sceneName);
+		}
+	}
+}
