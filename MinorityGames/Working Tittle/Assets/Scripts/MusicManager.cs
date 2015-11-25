0a1a8137367d0c92f7836cd426cec8d44e2c1206
Marcio <marciozkim@gmail.com>
2015-11-24 22:57:18 -0500

    Scripts

---
+++ MinorityGames/Working Tittle/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destrou on load : " + name);
+	}
+
+	void Start(){
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.loop = true;
+		audioSource.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnLevelWasLoaded( int level){
+
+		AudioClip clip = levelMusicChangeArray[level];
+
+		Debug.Log ("Playing clip: " + clip);
+
+		if (clip) {
+			audioSource.clip = clip;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
