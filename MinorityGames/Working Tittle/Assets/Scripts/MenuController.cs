e9da2427f851392a81ba6c437047ebce741ec3c2
Guelor Emanuel <ncstech07@gmail.com>
2016-01-05 12:14:07 -0500

    Added loading screen

--- MinorityGames/Working Tittle/Assets/Scripts/MenuController.cs
+++ MinorityGames/Working Tittle/Assets/Scripts/MenuController.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class MenuController : MonoBehaviour {
 
+    public GameObject loadingImage;
+
 	public void LoadScene(string sceneName){
 		if (sceneName.Equals ("Exit")) {
 			Debug.Log ("Quit Called");
 			Application.Quit ();
 		} else {
 			Debug.Log ("Load : " + sceneName);
+            loadingImage.SetActive(true);
 			Application.LoadLevel (sceneName);
 		}
 	}
