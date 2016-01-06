bae8f0edfe44bda5e62ebf310acb913aae2033e6
Guelor Emanuel <ncstech07@gmail.com>
2016-01-05 21:30:49 -0500

    Made the loading Screen asynchronous

--- MinorityGames/Working Tittle/Assets/Scripts/MenuController.cs
+++ MinorityGames/Working Tittle/Assets/Scripts/MenuController.cs
@@ -1,18 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
     public GameObject loadingImage;
+    public Slider loadingBar;
+    private AsyncOperation async;
+ 
+
+    IEnumerator LoadLevelWithBar(string sceneName)
+    {
+        async = SceneManager.LoadSceneAsync(sceneName);
+        while (!async.isDone){
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
 
 	public void LoadScene(string sceneName){
 		if (sceneName.Equals ("Exit")) {
 			Debug.Log ("Quit Called");
 			Application.Quit ();
 		} else {
-			Debug.Log ("Load : " + sceneName);
             loadingImage.SetActive(true);
-			Application.LoadLevel (sceneName);
+            StartCoroutine(LoadLevelWithBar(sceneName));
+			Debug.Log ("Load : " + sceneName);
+           
+			
 		}
 	}
 }
