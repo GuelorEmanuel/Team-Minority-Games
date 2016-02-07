e92bdf6dde6b4ba25a2ea9c06bb3ccc7db5169e4
Guelor Emanuel <ncstech07@gmail.com>
2016-02-07 04:36:53 -0500

    Restructured the directory, added health bar prefabs and animation

---
+++ MinorityGames/Working Tittle/Assets/Scripts/GUI/HealthBar.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	public float maxHealth     = 100f;
+	public float currentHealth = 0f;
+
+	public GameObject greenHealthBar;
+	public GameObject redHealthBar;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+		if (currentHealth > 0) {
+			InvokeRepeating ("decreaseHealth", 1f, 1f);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void decreaseHealth() {
+		currentHealth -= 2f;
+		float calculatedHealth = currentHealth / maxHealth; // if current health is 80 / 100 = 0.8f
+		setGreenHealthBar(calculatedHealth);
+	}
+
+	public void setGreenHealthBar(float health) {
+
+		//health vakue 0-1
+		greenHealthBar.transform.localScale = new Vector3 (Mathf.Clamp(health,0f,1f),
+			greenHealthBar.transform.localScale.y, greenHealthBar.transform.localScale.z);
+	}
+
+	public void setRedHealthBar(float health) { //@TODO need more logic for the red health bar decrease
+		redHealthBar.transform.localScale = new Vector3 (Mathf.Clamp(health,0f,1f),
+			redHealthBar.transform.localScale.y, redHealthBar.transform.localScale.z);
+		
+	}
+}
---
+++ MinorityGames/Working Tittle/Assets/Scripts/GUI/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject loadingImage;
+    public Slider loadingBar;
+    private AsyncOperation async;
+ 
+
+    IEnumerator LoadLevelWithBar(string sceneName)
+    {
+        async = SceneManager.LoadSceneAsync(sceneName);
+        while (!async.isDone){
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+
+	public void LoadScene(string sceneName)
+	{
+		if (sceneName.Equals ("Exit")) {
+			Debug.Log ("Quit Called");
+			Application.Quit ();
+		} else {
+            loadingImage.SetActive(true);
+            StartCoroutine(LoadLevelWithBar(sceneName));
+			Debug.Log ("Load : " + sceneName);
+		}
+	}
+}
