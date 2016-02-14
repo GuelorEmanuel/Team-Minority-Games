63be6e62d10e7a0ad1bb3f762c2845dc2751d32b
Marcio Zhu <Marcio Zhu>
2016-02-14 18:38:11 -0500

    Level Scene completed

---
+++ MinorityGames/Working Tittle/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public Image selectedArena;
+    public Sprite[] lvl_collection;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void ChangeArena(int index)
+    {
+        selectedArena.sprite = lvl_collection[index];
+    }
+}
