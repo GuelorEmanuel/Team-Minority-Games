9bbfd6580d74c1948113b5f3da754f3f2b2332d4
Guelor Emanuel <ncstech07@gmail.com>
2016-04-29 16:07:40 -0400

    Updated HealthBar Script, still need some work

--- MinorityGames/Working Tittle/Assets/Scripts/Player.cs
+++ MinorityGames/Working Tittle/Assets/Scripts/Player.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	
-	private HealthBar playerHealth;
+	 
+	private Animator animator;
+	private HealthBar playerHealthBar;
 	// Use this for initialization
 	void Start () {
+		animator = GetComponent<Animator> ();
+		playerHealthBar = GetComponent<HealthBar> ();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		playerHealthBar.setRedHealthBar (20f);
+		animator.SetInteger ("AnimState", 1);
 	}
 }
